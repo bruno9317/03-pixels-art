3cd8dfb566af50645aa66c9149682a34082a90b1
bruno <brunobaptista9317@gmail.com>
2022-08-31 16:17:45 -0300

    salvando boas praticas

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function recebeClick(){
+// Botão para gerar a paleta de cores aleatoriamente
+function recebeClick() {
+  const local1 = document.getElementById('cor2');
+  local1.style.backgroundColor = '#' + Math.floor(Math.random() * 1000000);
 
-    let local1 = document.getElementById("cor2");
-    local1.style.backgroundColor =  "#" + Math.floor(Math.random() * 1000000);
+  const local2 = document.getElementById('cor3');
+  local2.style.backgroundColor = '#' + Math.floor(Math.random() * 1000000);
 
-    let local2 = document.getElementById("cor3");
-    local2.style.backgroundColor =  "#" + Math.floor(Math.random() * 1000000);
-
-    let local3 = document.getElementById("cor4");
-    local3.style.backgroundColor =  "#" + Math.floor(Math.random() * 1000000);
+  const local3 = document.getElementById('cor4');
+  local3.style.backgroundColor = '#' + Math.floor(Math.random() * 1000000);
 }
 
-let butao = document.getElementById("button-random-color");
-butao.addEventListener("click", recebeClick);
+const butao = document.getElementById('button-random-color');
+butao.addEventListener('click', recebeClick);
+// Botão para gerar a paleta de cores aleatoriamente
