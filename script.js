222c9e4f1855165533ac4d2bdfd282fccababbb1
bruno <brunobaptista9317@gmail.com>
2022-09-02 12:49:11 -0300

    atualizando ate o requisito 11

--- script.js
+++ script.js
@@ -21,22 +21,6 @@ const butao = document.getElementById('button-random-color');
 butao.addEventListener('click', recebeClick);
 // Botão para gerar a paleta de cores aleatoriamente e salva no localStorage
 
-// Função para carregar as cores que estavam na paleta quando a aba foi fechada pela ultima vez
-window.onload = function () {
-    document.getElementById('cor1').style.backgroundColor = "black";
-    let aqui = "";
-    if(localStorage.length != 0){
-     aqui = localStorage.getItem('colorPalette').split("@");
-    }
-    let savedCor2 = aqui[0];
-    let savedCor3 = aqui[1];
-    let savedCor4 = aqui[2];
-    document.getElementById('cor2').style.backgroundColor = savedCor2;
-    document.getElementById('cor3').style.backgroundColor = savedCor3;
-    document.getElementById('cor4').style.backgroundColor = savedCor4;
-}
-// Função para carregar as cores que estavam na paleta quando a aba foi fechada pela ultima vez
-
 
 // designa a classe "selected" a cor clicada e tira a classe da cor anterior
 document.getElementById('cor1').classList.add("selected");
@@ -44,7 +28,6 @@ document.getElementById('cor1').classList.add("selected");
 function recebeClick2(e) {
     document.getElementsByClassName('selected')[0].classList.remove('selected');
     e.target.classList.add("selected");
-    // document.getElementsByClassName('selected')[0].style.backgroundColor;
 }
 // designa a classe "selected" a cor clicada e tira a classe da cor anterior
 
@@ -60,14 +43,26 @@ for(let index = 0; index < listaClassColor.length; index += 1){
 criaButao();
 // transforma a paleta de cores em botoes
 
-// o pixel que for clicado recebe backgroundColor atual do elemento na class 'selected'
+// o pixel que for clicado recebe backgroundColor atual do elemento na class 'selected' e é salva no storage
 function recebeClick3(e){
     e.target.style.backgroundColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+    let aqui = [];
+    let listaDePixels2 = document.getElementsByClassName('pixel');
+    for(let index = 0; index < listaDePixels2.length; index += 1){
+        aqui[index] = listaDePixels2[index].style.backgroundColor + "@";
+    }
+    localStorage.setItem('pixelBoard', aqui);
+
 }
-// o pixel que for clicado recebe backgroundColor atual do elemento na class 'selected'
+// o pixel que for clicado recebe backgroundColor atual do elemento na class 'selected'e é salva no storage
 
 // transforma os pixels em butao
 function criaButao2() {
+    let listaDePixels2 = document.getElementsByClassName('pixel');
+    for(let index = 0; index < listaDePixels2.length; index += 1){
+        listaDePixels2[index].style.backgroundColor = '#ffffff';
+    }
+
     let listaDePixel = document.getElementsByClassName('pixel');
     let botoesPixel = [];
     for(let index = 0; index < listaDePixel.length; index += 1){
@@ -78,12 +73,62 @@ function criaButao2() {
 criaButao2();
 // transforma os pixels em butao
 
-// cria o botao 'Limpar' que chama uma função que deixa todos os pixels brnacos
+// cria o botao 'Limpar' que chama uma função que deixa todos os pixels bracos
 const butaoLimpa = document.getElementById('clear-board');
 butaoLimpa.addEventListener('click', function () {
     const listaDePixels = document.getElementsByClassName('pixel');
+    console.log(listaDePixels);
     for(let index = 0; index < listaDePixels.length; index += 1){
-        listaDePixels[index].style.backgroundColor = 'white';
+        listaDePixels[index].style.backgroundColor = '#ffffff';
     }
+
+    let aqui = [];
+    let listaDePixels2 = document.getElementsByClassName('pixel');
+    for(let index = 0; index < listaDePixels2.length; index += 1){
+        aqui[index] = listaDePixels2[index].style.backgroundColor + "@";
+    }
+    
+    localStorage.setItem('pixelBoard', aqui);
 })
-// cria o botao 'Limpar' que chama uma função que deixa todos os pixels brnacos
+// cria o botao 'Limpar' que chama uma função que deixa todos os pixels bracos
+
+
+// Função para carregar as cores que estavam na paleta quando a aba foi fechada pela ultima vez
+window.onload = function () {
+    document.getElementById('cor1').style.backgroundColor = "rgb(0, 0, 0";
+    document.getElementById('cor2').style.backgroundColor = "rgb(255, 0, 0";
+    document.getElementById('cor3').style.backgroundColor = "rgb(0, 0, 255";
+    document.getElementById('cor4').style.backgroundColor = "rgb(0, 128, 0";
+    let aqui = "";
+    if(localStorage.length != 0){
+     aqui = localStorage.getItem('colorPalette').split("@");
+    }
+    let savedCor2 = aqui[0];
+    let savedCor3 = aqui[1];
+    let savedCor4 = aqui[2];
+    document.getElementById('cor2').style.backgroundColor = savedCor2;
+    document.getElementById('cor3').style.backgroundColor = savedCor3;
+    document.getElementById('cor4').style.backgroundColor = savedCor4;
+    
+    // pula pula pula pula 
+    if(localStorage.getItem('pixelBoard') !== null){
+        let aqui2 = "";
+        aqui2 = localStorage.getItem('pixelBoard').split("@");
+        let listaDePixels = document.getElementsByClassName('pixel');
+        for(let index = 1; index <= listaDePixels.length; index += 1){
+            aqui2[index] = aqui2[index].replace(",", "");
+        }
+        aqui2.length = aqui2.length - 1;
+        for(let index = 0; index < listaDePixels.length; index += 1){
+            listaDePixels[index].style.backgroundColor = aqui2[index];
+        }
+        }else if(localStorage.getItem('pixelBoard') == null){
+        let listaDePixels2 = document.getElementsByClassName('pixel');
+        for(let index = 0; index < listaDePixels2.length; index += 1){
+            listaDePixels2[index].style.backgroundColor = '#ffffff';
+        }
+    }
+    
+    // aqui2 = JSON.stringify(aqui2)
+}
+// Função para carregar as cores que estavam na paleta quando a aba foi fechada pela ultima vez
